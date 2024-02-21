23b69dad8385bd99bffbfba970ec348eda6d05fa
Laís Domingos dos Santos <50579591+LaisDomingos@users.noreply.github.com>
2024-02-21 09:05:10 -0300

    Add files via upload

--- twtodos/setup/urls.py
+++ twtodos/setup/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import TodoListView, TodoCrateView
+from todos.views import TodoListView, TodoCrateView, TodoUpdateView, TodoDeleteView  
 urlpatterns = [
     path("admin/", admin.site.urls), 
     path("", TodoListView.as_view(), name="todo_list"), 
-    path("create", TodoCrateView.as_view(), name="todo_create"), 
+    path("create", TodoCrateView.as_view(), name="todo_create"),
+    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
+    path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
 ]
--- twtodos/todos/views.py
+++ twtodos/todos/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
 
 from .models import Todo
@@ -10,3 +10,15 @@ class TodoCrateView(CreateView):
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
+
+#class TodoCompleteView(View):
+    #def get(self, request):
